b6d1f46674c2680055b25ee0dad2ed1ab15b1a56
Mohakim231 <115846960+Mohakim231@users.noreply.github.com>
2023-02-09 16:10:08 +0000

    Update diary.js

--- server/controllers/diary.js
+++ server/controllers/diary.js
@@ -19,16 +19,6 @@ async function show (req, res) {
     }
 }
 
-async function getTop (req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const diary = await Diary.getTopSnack(id);
-        res.status(200).json(diary);
-    } catch (err) {
-        res.status(404).json({"error": err.message})
-    }
-}
-
 async function create (req, res) {
     try {
         const data = req.body;
